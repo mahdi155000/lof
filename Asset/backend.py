e3cb4c5b7cc6235b29f5e840f25a5c1a6b98185b
mahdi <mahdi155000@gmail.com>
2023-04-26 13:24:08 +0330

    fix program nameing to PEP8

---
+++ Asset/backend.py
@@ -0,0 +1,77 @@
+#               IN THE NAME OF GOD
+import sqlite3
+import shutil
+from os import path
+
+PATH = path.dirname(__file__)
+PATH = PATH + path.sep
+
+
+def connect():
+    conn = sqlite3.connect(PATH + "list_of_work.db")
+    cur = conn.cursor()
+    cur.execute(
+        "CREATE TABLE IF NOT EXISTS lof (id INTEGER PRYMARY KEY, title TEXT, value_ BLOB, constant text, comment TEXT)")
+    conn.commit()
+    conn.close()
+
+
+def insert(titile='', value_='', constant='', comment=''):
+    conn = sqlite3.connect(PATH + "list_of_work.db")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO lof VALUES (NULL, ?, ?, ?, ?)", (titile, value_, constant, comment))
+    conn.commit()
+    conn.close()
+
+
+def view():
+    conn = sqlite3.connect(PATH + "list_of_work.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM lof")
+    row = cur.fetchall()
+    conn.close()
+    return row
+
+
+def search(title, value_, comment, constant):
+    conn = sqlite3.connect(PATH + "list_of_work.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM lof WHERE title=? OR value_=? OR constant=? OR comment=?",
+                (title, value_, constant, comment))
+    row = cur.fetchall()
+    conn.close()
+    return row
+
+
+def delete(id):
+    conn = sqlite3.connect(PATH + "list_of_work.db")
+    cur = conn.cursor()
+    cur.execute("DELETE FROM lof WHERE id=?", (id,))
+    conn.commit()
+    conn.close()
+
+
+def update(id, title='', value_='', constant='', comment=''):
+    conn = sqlite3.connect(PATH + "list_of_work.db")
+    cur = conn.cursor()
+    cur.execute("UPDATE lof SET  title=?, value_=?, constant=?, comment=? WHERE id=?",
+                (title, value_, constant, comment, id))
+    conn.commit()
+    conn.close()
+
+
+def update_id(last_id, new_id):
+    conn = sqlite3.connect(PATH + "list_of_work.db")
+    cur = conn.cursor()
+    cur.execute("UPDATE lof SET id=? WHERE id=?", (new_id, last_id))
+    conn.commit()
+    conn.close()
+
+
+connect()
+# print(view())
+# insert('Movies',1,'S04',"This is a movies")
+# insert('Movies',0,"S04",'this is a movies')
+# delete()
+# update(1,'halo', 3, 'S02','This is a film')
+# update_id(4,3)
--- main.py
+++ main.py
@@ -8,40 +8,42 @@ print(termcolor2.colored(pyfiglet.figlet_format("IN THE NAME OF GOD"), 'red'))
 
 # print(termcolor2.colored(pyfiglet.figlet_format('IN THE NAME OF GOD'), 'red'))
 
+def plus(item):
+    M_L[item - 1][2] += 1
+
+
+def show_item(do_what='plus'):
+    num = 0
+    for work in M_L:
+        num += 1
+        if num % 2 == 0:
+            color = 'blue'
+        else:
+            color = 'red'
+        l = len(str(work[1]))
+        os_tab_number = 6
+        # if os.name == 'nt':
+        #     os_tab_number = 4
+        # print("I change tab number for Windows")
+        if l <= os_tab_number:
+            text = str(num) + f') for  {doWhat} one ' + f'"{work[1]}": \t\t\t\t\t"{work[2]}'
+            # text = str(num) + f') For {doWhat} one ' + f'"{work[1]}":\t\t\t\t\t"{work[2]}"'
+        elif l <= os_tab_number * 2:
+            text = str(num) + f') for  {doWhat} one ' + f'"{work[1]}": \t\t\t\t"{work[2]}'
+        elif l <= os_tab_number * 3:
+            text = str(num) + f') for  {doWhat} one ' + f'"{work[1]}": \t\t\t"{work[2]}'
+        elif l <= os_tab_number * 4:
+            text = str(num) + f') for  {doWhat} one ' + f'"{work[1]}": \t\t"{work[2]}'
+        elif l <= os_tab_number * 5:
+            text = str(num) + f') for  {doWhat} one ' + f'"{work[1]}": \t"{work[2]}'
+        elif l > os_tab_number * 5:
+            text = str(num) + f') for  {doWhat} one ' + f'"{work[1]}": "{work[2]}'
+        print(termcolor2.colored(text, color))
+
+
 class Action:
     def __init__(self):
         self.M_L = M_L
-
-    def plus(self, item):
-        M_L[item - 1][2] += 1
-
-    def showItem(self, doWhat='plus'):
-        num = 0
-        for work in M_L:
-            num += 1
-            if num % 2 == 0:
-                color = 'blue'
-            else:
-                color = 'red'
-            l = len(str(work[1]))
-            os_tab_number = 6
-            # if os.name == 'nt':
-            #     os_tab_number = 4
-            # print("I change tab number for Windows")
-            if l <= os_tab_number:
-                text = str(num) + f') for  {doWhat} one ' + f'"{work[1]}": \t\t\t\t\t"{work[2]}'
-                # text = str(num) + f') For {doWhat} one ' + f'"{work[1]}":\t\t\t\t\t"{work[2]}"'
-            elif l <= os_tab_number * 2:
-                text = str(num) + f') for  {doWhat} one ' + f'"{work[1]}": \t\t\t\t"{work[2]}'
-            elif l <= os_tab_number * 3:
-                text = str(num) + f') for  {doWhat} one ' + f'"{work[1]}": \t\t\t"{work[2]}'
-            elif l <= os_tab_number * 4:
-                text = str(num) + f') for  {doWhat} one ' + f'"{work[1]}": \t\t"{work[2]}'
-            elif l <= os_tab_number * 5:
-                text = str(num) + f') for  {doWhat} one ' + f'"{work[1]}": \t"{work[2]}'
-            elif l > os_tab_number * 5:
-                text = str(num) + f') for  {doWhat} one ' + f'"{work[1]}": "{work[2]}'
-            print(termcolor2.colored(text, color))
 
 
 M_L = [[1, "test", 15], [2, "are you OK", 52]]
@@ -52,7 +54,7 @@ work_counter = 1
 # for work in M_L:
 #     print(f"{work_counter}) {work[1]}")
 #     work_counter += 1
-acls.showItem("plus")
+show_item("plus")
 print("enter 'c' for go to command mode.")
 
 while True:
