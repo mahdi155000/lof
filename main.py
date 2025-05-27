3ef6dadf0e0047dc44c7752e8f86f8906f43cbd7
mahdi <mahdi155000@gmail.com>
2025-05-27 20:25:45 +0330

    makeing main.py file cleaner and add some comment using AI.

--- main.py
+++ main.py
@@ -9,165 +9,134 @@ from Asset.plugins import exit_list
 from plugin import plugins
 from workspace_manager_module import workspace_manager
 
+# --- Constants and Path Setup ---
 if os.name == 'nt':
     os.system('color')
+
 print(termcolor2.colored(pyfiglet.figlet_format("IN THE NAME OF GOD"), 'red'))
 
-if os.path.dirname(__file__) == '':
-    PATH = '.' + os.path.sep
-else:
-    PATH = os.path.dirname(__file__)
-    PATH = PATH + os.path.sep
+PATH = os.path.dirname(__file__) + os.path.sep if os.path.dirname(__file__) else '.' + os.path.sep
 
 ENCRYPTED_DATABASE = False
 if os.path.isfile(PATH + f"Asset{os.sep}config.py"):
     from Asset import config  # pylint: disable = E0611,C0412
-
     ENCRYPTED_DATABASE = True
     if os.path.isfile(PATH + f"Asset{os.sep}list_of_work.db.gpg"):
         config.decrypt()
 
+# --- Workspace Setup ---
 workspace_manager.switch_workspace('lof')
 
-
+# --- Plugin Importer ---
 def import_plugins():
-    """Import all plugins exist in Asset/plugins folder and get program access to them"""
-    plugins = {}  # pylint: disable=W0621
-    # the plugins directory path
-    plugins_dir = "Asset.plugins"  # change to directory
-
-    # make a list from all files inside the plugins directory
-    for filename in os.listdir(f"{PATH}Asset{os.sep}plugins"):  # directory path
+    """Import all plugins from Asset/plugins folder and return as a dict."""
+    plugins_dict = {}
+    plugins_dir = "Asset.plugins"
+    for filename in os.listdir(f"{PATH}Asset{os.sep}plugins"):
         if filename.endswith(".py") and filename != "__init__.py":
-            module_name = filename[:-3]  # remove '.py' part from the name
-            module = importlib.import_module(
-                f'{plugins_dir}.{module_name}')  # use module name
-            plugins[module_name] = module
-    return plugins
+            module_name = filename[:-3]
+            module = importlib.import_module(f'{plugins_dir}.{module_name}')
+            plugins_dict[module_name] = module
+    return plugins_dict
 
+# --- Load Plugins ---
+all_plugins = import_plugins()
 
-import_plugins()
+# --- Main List Initialization ---
+def refresh_main_list():
+    backend.fill_list(backend.view(workspace_manager.current_workspace))
+    return backend.view(workspace_manager.current_workspace)
 
-M_L = []
-backend.fill_list(backend.view(workspace_manager.current_workspace))
-M_L = backend.view(workspace_manager.current_workspace)
+M_L = refresh_main_list()
 print("---------------------------------------------")
 print(M_L)
 print("---------------------------------------------")
 plugins["show"]()
 
+# --- Command Handlers ---
+def handle_number_command(lNumber, M_L):
+    try:
+        idx = abs(lNumber) - 1
+        add_number = 1 if lNumber >= 0 else -1
+        li = M_L[idx]
+        backend.update(
+            li[0], title=li[1], value=(int(li[2]) + add_number),
+            constant=li[3], comment=li[4], workspace=workspace_manager.current_workspace
+        )
+        return True
+    except IndexError:
+        print("Your input number is out of range")
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+    return False
+
+def handle_plugin_command(command, plugins):
+    try:
+        if command in plugins:
+            plugins[command]()
+            return True
+        parts = command.split()
+        main_command = parts[0]
+        sub_command = parts[1] if len(parts) > 1 else None
+        if main_command in plugins:
+            plugin = plugins[main_command]
+            if sub_command and hasattr(plugin, sub_command):
+                getattr(plugin, sub_command)()
+            elif sub_command and isinstance(plugin, dict) and sub_command in plugin:
+                plugin[sub_command]()
+            else:
+                print(f"Invalid subcommand: {sub_command}")
+            return True
+    except Exception as e:
+        print('Issue from running plugin command. Please report it to programmer.')
+        print(e)
+    return False
+
+def handle_exit():
+    if ENCRYPTED_DATABASE:
+        config.encrypt()
+        db_path = PATH + f"Asset{os.sep}list_of_work.db"
+        if os.path.isfile(db_path):
+            os.remove(db_path)
+    elif not os.path.isfile(PATH + f"Asset{os.sep}list_of_work.db.gpg") and os.path.isfile(PATH + f"Asset{os.sep}config.py"):
+        config.encrypt()
+    sys.exit()
+
+# --- Main Loop ---
 while True:
     try:
-        # Attempt to get the user input
         what_to_do = input("->:\n").lower()
     except KeyboardInterrupt:
         print("\nOK I will exit the program.")
         sys.exit()
 
-    int_check_var = False  # pylint: disable=invalid-name
-    current_workspace = workspace_manager.get_workspace()  # Get the current workspace
+    int_check_var = False
     show_status = True
 
-    try:
-        if what_to_do.isdigit():
-            lNumber = int(what_to_do)
-            if lNumber >= 0:
-                add_number = 1  # pylint: disable=invalid-name
-                li = M_L[lNumber - 1]
-                backend.update(li[0], title=li[1], value=(int(li[2]) + add_number),
-                               constant=li[3], comment=li[4], workspace=workspace_manager.current_workspace)
-                # M_L[lNumber - 1][2] += 1
-            elif lNumber < 0:
-                lNumber = abs(lNumber)
-                add_number = -1  # pylint: disable=invalid-name
-                li = M_L[lNumber - 1]
-                backend.update(li[0], title=li[1], value=(int(li[2]) + add_number),
-                               constant=li[3], comment=li[4], workspace=workspace_manager.current_workspace)
-                # M_L[lNumber - 1][2] -= 1
-            int_check_var = True  # pylint: disable=invalid-name
-        else:
-            pass
-    except IndexError:
-        print("Your input number is out of range")
-    except ValueError:
-        print("Invalid input. Please enter a number.")
-
-    if int_check_var:
-        pass
+    # Handle numeric commands
+    if what_to_do.isdigit() or (what_to_do.startswith('-') and what_to_do[1:].isdigit()):
+        lNumber = int(what_to_do)
+        int_check_var = handle_number_command(lNumber, M_L)
     elif what_to_do == '':
         pass
-    elif "what_to_do" not in globals():
-        continue
     elif what_to_do in exit_list:
-        if ENCRYPTED_DATABASE:
-            config.encrypt()
-            os.remove(PATH + f"Asset{os.sep}list_of_work.db")
-        elif not os.path.isfile(PATH + f"Asset{os.sep}list_of_work.db.gpg") and os.path.isfile(PATH + f"Asset{os.sep}config.py"):
-            config.encrypt()
-        sys.exit()
-    # elif what_to_do == "show":
-    #     new_show()
-    elif what_to_do in plugins:
-        try:
-            plugins[what_to_do]()
-        except TypeError:
-            command = what_to_do.strip().split()
-            main_command = command[0]
-            sub_command = command[1] if len(command) > 1 else None
-
-            if main_command in plugins:
-                if sub_command and sub_command in plugins[main_command]:
-                    plugins[main_command][sub_command]()
-                else:
-                    print(
-                        f"Subcommands for {main_command}: {', '.join(plugins[main_command].keys())}")
-        except KeyError as e:
-            print("You are not entering the correct information. Please try again!")
-            print(e)
-            # print(e)
-        except KeyboardInterrupt:
-            print("\nKeyboardInterrupt detected. OK I will exit switching mode.")
-            continue
-    # elif what_to_do.split()[0] in plugins:
-    #     try:
-    #         main_command = what_to_do.split()[0]
-    #         sub_command = what_to_do.split()[1] if len(
-    #             what_to_do.split()) > 1 else None
-
-    #         if sub_command and sub_command in plugins[main_command]:
-    #             plugins[main_command][sub_command]()
-    #         else:
-    #             print(f"Invalid subcommand: {sub_command}")
-    #     except:
-    #         print('issue from running subcommand. please report it to programmer')
-    elif what_to_do.split()[0] in plugins:
-        try:
-            main_command = what_to_do.split()[0]
-            sub_command = what_to_do.split()[1] if len(
-                what_to_do.split()) > 1 else None
-
-            if sub_command and sub_command in plugins[main_command]:
-                plugins[main_command][sub_command]()  # Run subcommand if valid
-            else:
-                print(f"Invalid subcommand: {sub_command}")
-        except Exception as e:
-            print('Issue from running subcommand. Please report it to programmer.')
-            print(e)
+        handle_exit()
+    elif handle_plugin_command(what_to_do, plugins):
+        pass
+    elif handle_plugin_command(what_to_do, all_plugins):
+        pass
     else:
-        try:
-            print("Your command is not supported")
-        except Exception as e:
-            print(e)
+        print("Your command is not supported")
 
     # Refresh M_L with the current workspace data
     try:
-        backend.fill_list(backend.view(workspace_manager.current_workspace))
-        M_L = backend.view(workspace_manager.current_workspace)
+        M_L = refresh_main_list()
     except Exception as e:
         print(f"An error occurred: {e}")
 
+    # Show updated item if applicable
     try:
-        if show_status:
-            print(M_L[lNumber - 1])
-    except Exception as e:
+        if int_check_var and 'lNumber' in locals():
+            print(M_L[abs(lNumber) - 1])
+    except Exception:
         pass
