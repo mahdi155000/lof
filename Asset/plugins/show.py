f11f42891fa99b7ad66bc706bfcb118b0f7bcd92
mahdi <mahdi155000@gmail.com>
2025-12-15 20:50:45 +0330

    update show and show item to support json format

--- Asset/plugins/show.py
+++ Asset/plugins/show.py
@@ -1,6 +1,7 @@
 from plugin import plugin
 from Asset import backend
 import termcolor2
+import json
 from workspace_manager_module import workspace_manager
 
 
@@ -9,15 +10,26 @@ def show_function(do_what='plus'):
     M_L = backend.view(workspace_manager.current_workspace)
     id_counter = 1
     for item in M_L:
-        if item[0] % 2:
-            show_color = 'blue'
+        title = item[1]
+        value = item[2]
+        constant = item[3]
+
+        # Determine last episode if it's a JSON map
+        if constant == "episodes":
+            try:
+                ep_map = json.loads(value)
+                # Find the highest season and last episode
+                last_season = max(int(s) for s in ep_map.keys())
+                last_episode = max(ep_map[str(last_season)])
+                display_value = f"S{last_season}E{last_episode}"
+            except Exception:
+                display_value = value
         else:
-            show_color = 'red'
-        if item[0] < 10:
-            text = (" {:<1}) for {} one {:<45} {}".format(
-                id_counter, do_what, item[1], item[2]))
-        else:
-            text = ("{:<1}) for {} one {:<45} {}".format(
-                id_counter, do_what, item[1], item[2]))
+            display_value = value
+
+        # Coloring
+        show_color = 'blue' if item[0] % 2 else 'red'
+
+        text = f"{id_counter}) for {do_what} one {title:<45} {display_value}"
         print(termcolor2.colored(text, show_color))
         id_counter += 1
--- Asset/plugins/show_item.py
+++ Asset/plugins/show_item.py
@@ -1,13 +1,27 @@
 from plugin import plugin
 from Asset import backend
 import termcolor2
+import json
 from workspace_manager_module import workspace_manager
 
 
 @plugin("show item")
 def show_item():
-    """for now it has to be change
-    """
+    """Show full details of each item, including full episode map if present"""
     M_L = backend.view(workspace_manager.current_workspace)
-    for work in M_L:
-        print(work)
+    for item in M_L:
+        title = item[1]
+        value = item[2]
+        constant = item[3]
+
+        if constant == "episodes":
+            try:
+                ep_map = json.loads(value)
+                display_value = json.dumps(ep_map, indent=2)
+            except Exception:
+                display_value = value
+        else:
+            display_value = value
+
+        text = f"{title:<30} | {display_value}"
+        print(termcolor2.colored(text, 'cyan'))
